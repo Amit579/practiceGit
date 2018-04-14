e8a4e01c839ef8e5e65518c959f8736e83f21661
Ashwani Sharma <ashwanisharma@Ashwanis-MacBook-Air.local>
2018-04-14 10:39:08 +0530

    new

---
+++ sampleGit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sampleGit
+//
+//  Created by Ashwani Sharma on 4/14/18.
+//  Copyright © 2018 TestSelf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+typealias fullName = String
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let firstName : fullName = "Check Git"
+        print(firstName)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
